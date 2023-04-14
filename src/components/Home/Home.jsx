56f765d26e51cc65fa18fb078a92dd276d969464
Toushik <tayebhossain018@gmail.com>
2023-04-14 14:49:15 +0600

    added some components

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,20 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useLoaderData } from 'react-router-dom';
+import TShirt from '../TShirt/TShirt';
+import Cart from '../Cart/Cart';
+import './Home.css'
 
 const Home = () => {
     const tShirts = useLoaderData();
-    // console.log(tShirts)
+
+    const [cart, setCart] = useState([]);
+
+    const handleAddToCart = tShirt =>{
+        console.log(tShirt)
+    }
+    
     return (
-        <div>
-            <h1>{tShirts.length}</h1>
+        <div className='home-container'>
+           <div className='t-shirts-container'>
           {
-              tShirts.map(tShirt => 
-                <div key={tShirt._id}>
-                    
-                    <img src={tShirt.picture} alt="" />
-                </div>
-                )
+              tShirts.map(tShirt => <TShirt
+              key={tShirt._id}
+              tShirt={tShirt}
+              handleAddToCart={handleAddToCart}
+              ></TShirt>)
           }
+           </div>
+           <div className='cart-container'>
+            <Cart></Cart>
+
+           </div>
 
         </div>
     );
